61b9be4ab4e24214182939ceb7a034f5c7be0778
EIREXE <eirexe123@gmail.com>
2013-06-08 15:34:46 +0200

    Added luas (NPC AND PLAYERMODEL)

---
+++ Sunny(MetalGearRising)/lua/autorun/sunny_npc.lua
@@ -0,0 +1,11 @@
+local Category = "Metal Gear Rising"
+local NPC = {
+		 		Name = "Sunny", 
+				Class = "npc_citizen",
+				KeyValues = { citizentype = 4 },
+				Model = "models/EIREXE/sunnyfinal3.mdl",
+				Health = "250",
+				Category = Category	
+		}
+
+list.Set( "NPC", "npc_sunny", NPC )
---
+++ Sunny(MetalGearRising)/lua/autorun/sunny_pm.lua
@@ -0,0 +1,6 @@
+if (SERVER) then
+player_manager.AddValidModel( "sunny", "models/EIREXE/sunnyfinal3.mdl" ) 
+end
+AddCSLuaFile( "sunny_pm.lua" )
+list.Set( "PlayerOptionsModel", "sunny", 		"models/EIREXE/sunnyfinal3.mdl" )
+
