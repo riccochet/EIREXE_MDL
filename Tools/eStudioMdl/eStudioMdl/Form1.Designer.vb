87a231025707919e599f4827f44449a4ee96fd72
EIREXE <eirexe123@gmail.com>
2013-06-09 18:52:13 +0200

    Added a new tool, eStudioMdl

---
+++ Tools/eStudioMdl/eStudioMdl/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChooseQCToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SetSFMStudiomdlPathToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SetGamepathToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConsoleOut = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "SFM"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ToolsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(699, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ChooseQCToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ChooseQCToolStripMenuItem
+        '
+        Me.ChooseQCToolStripMenuItem.Name = "ChooseQCToolStripMenuItem"
+        Me.ChooseQCToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ChooseQCToolStripMenuItem.Text = "Choose QC"
+        '
+        'ToolsToolStripMenuItem
+        '
+        Me.ToolsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SetSFMStudiomdlPathToolStripMenuItem, Me.SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem, Me.SetGamepathToolStripMenuItem})
+        Me.ToolsToolStripMenuItem.Name = "ToolsToolStripMenuItem"
+        Me.ToolsToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.ToolsToolStripMenuItem.Text = "Tools"
+        '
+        'SetSFMStudiomdlPathToolStripMenuItem
+        '
+        Me.SetSFMStudiomdlPathToolStripMenuItem.Name = "SetSFMStudiomdlPathToolStripMenuItem"
+        Me.SetSFMStudiomdlPathToolStripMenuItem.Size = New System.Drawing.Size(296, 22)
+        Me.SetSFMStudiomdlPathToolStripMenuItem.Text = "Set SFM studiomdl path"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem
+        '
+        Me.SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem.Name = "SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem"
+        Me.SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem.Size = New System.Drawing.Size(296, 22)
+        Me.SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem.Text = "Set SteamPipe Source SDK studiomdl path"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 19)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(84, 17)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Xbox Check"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'SetGamepathToolStripMenuItem
+        '
+        Me.SetGamepathToolStripMenuItem.Name = "SetGamepathToolStripMenuItem"
+        Me.SetGamepathToolStripMenuItem.Size = New System.Drawing.Size(296, 22)
+        Me.SetGamepathToolStripMenuItem.Text = "Set Gamepath"
+        '
+        'ConsoleOut
+        '
+        Me.ConsoleOut.Location = New System.Drawing.Point(12, 27)
+        Me.ConsoleOut.Name = "ConsoleOut"
+        Me.ConsoleOut.Size = New System.Drawing.Size(675, 323)
+        Me.ConsoleOut.TabIndex = 4
+        Me.ConsoleOut.Text = ""
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(487, 356)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Compile"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 366)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Extra Stuff at compiling"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(88, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "SteamPipe"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(699, 535)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ConsoleOut)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "eStudioMdl"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ChooseQCToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SetSFMStudiomdlPathToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents SetGamepathToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ConsoleOut As System.Windows.Forms.RichTextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ Tools/eStudioMdl/eStudioMdl/Form1.vb
@@ -0,0 +1,172 @@
+﻿
+Public Class Form1
+    Public xbox As Boolean
+    Public sfmpath As String
+    Public hl2path As String
+    Public qcpath As String
+    Public gamepath As String
+    Public appath As String
+    Dim tempconsole As String
+
+
+    Function compileQC(ByVal studiomdlversion As String)
+        Dim studiomdlpath As String
+        Dim argumentst As String
+        Dim xboxtarget As String
+        'if studiomdl target (specified by arguments) is sfm then we set studiomdl path to sfm
+        If studiomdlversion = "sfm" Then
+            studiomdlpath = sfmpath
+
+        End If
+        'if hl2 is target then use it
+        If studiomdlversion = "Hl2" Then
+            studiomdlpath = hl2path
+        End If
+        'if xbox is checked then we decide wether to include xbox parameter or not
+        If xbox = True Then
+            'no need to run any special argument, since studiomdl compiles xbox by default
+            xboxtarget = ""
+        End If
+        'same for noxbox
+        If xbox = False Then
+            xboxtarget = "-noxbox"
+        End If
+
+        'build the path
+        argumentst = xboxtarget & " " & "-game" & " " & gamepath & " " & qcpath
+
+        'run the process
+        Dim runprocess As New Process()
+        Try
+
+            runprocess.StartInfo.FileName = studiomdlpath
+            runprocess.StartInfo.UseShellExecute = False
+            runprocess.StartInfo.Arguments = argumentst
+            runprocess.StartInfo.CreateNoWindow = False
+            runprocess.StartInfo.RedirectStandardOutput = True
+            runprocess.Start()
+            runprocess.WaitForExit()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        ConsoleOut.Text = runprocess.StandardOutput.ReadToEnd
+
+        Dim FileEx As String = runprocess.StandardOutput.ReadToEnd
+
+
+
+        If FileEx.Contains("ERROR") Or FileEx.Contains("Error") Then
+            MsgBox("Error Happened")
+        Else
+            MsgBox("Non errors hapenned")
+        End If
+
+
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        '??????
+        appath = Application.StartupPath
+        tempconsole = appath & "/temp.txt"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Some tests before running studiomdl running function in sfm mode
+        Dim sfmtest As Boolean
+        Dim qctest As Boolean
+        'Check if all the paths are OK, we don't want to launch a broken command
+        If Not qcpath = "" Then
+            qctest = True
+        Else
+            MsgBox("Oops! Qc Path is empty")
+            Exit Sub
+        End If
+        If Not sfmpath = "" Then
+            sfmtest = True
+        Else
+            MsgBox("Oops! Sfm studiomdl path is empty")
+            Exit Sub
+        End If
+        If sfmtest And qctest = True Then
+            MsgBox("Both tests passed!")
+            compileQC("sfm")
+        End If
+
+
+
+
+
+    End Sub
+
+
+    Private Sub SetSFMStudiomdlPathToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetSFMStudiomdlPathToolStripMenuItem.Click
+        'Studiomdl selection
+        OpenFileDialog1.Filter = "exe files (*.exe)|*.exe"
+        OpenFileDialog1.ShowDialog()
+        sfmpath = Chr(34) & OpenFileDialog1.FileName & Chr(34)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        'Xbox check
+        If CheckBox1.CheckState = CheckState.Indeterminate Then
+            xbox = True
+        End If
+        If CheckBox1.CheckState = CheckState.Unchecked Then
+            xbox = False
+        End If
+    End Sub
+
+    Private Sub ChooseQCToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ChooseQCToolStripMenuItem.Click
+        'Build Qc path based on user input and add quotation marks
+        Dim opendialogqc As New OpenFileDialog
+        opendialogqc.Filter = "Valve QC Files (*.qc)|*.qc"
+        opendialogqc.ShowDialog()
+        Try
+            qcpath = Chr(34) & opendialogqc.FileName & Chr(34)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub SetGamepathToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetGamepathToolStripMenuItem.Click
+        ' here we build the gamefolder path
+        Dim openfoldergame As New FolderBrowserDialog
+
+        openfoldergame.ShowDialog()
+
+        gamepath = Chr(34) & openfoldergame.SelectedPath & Chr(34)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Some tests before running studiomdl running function in sfm mode
+        Dim sfmtest As Boolean
+        Dim qctest As Boolean
+        'Check if all the paths are OK, we don't want to launch a broken command
+        If Not qcpath = "" Then
+            qctest = True
+        Else
+            MsgBox("Oops! Qc Path is empty")
+            Exit Sub
+        End If
+        If Not hl2path = "" Then
+            sfmtest = True
+        Else
+            MsgBox("Oops! Sfm studiomdl path is empty")
+            Exit Sub
+        End If
+        If sfmtest And qctest = True Then
+            MsgBox("Both tests passed!")
+            compileQC("Hl2")
+        End If
+
+    End Sub
+
+    Private Sub SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SetSteamPipeSourceSDKStudiomdlPathToolStripMenuItem.Click
+        Dim opensteampipe As New OpenFileDialog
+        opensteampipe.Filter = "exe files (*.exe)|*.exe"
+        opensteampipe.ShowDialog()
+        hl2path = opensteampipe.FileName
+    End Sub
+End Class
